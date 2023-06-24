374c19474e3022c2587c3e1eaa2271fe6b5a20a6
Yohan Sandun Kumaranayake <84405840+Dishan-Chalana@users.noreply.github.com>
2023-06-24 22:08:36 +0530

    Navbar created

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,21 @@
+import './navbar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Refresh</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavBar;
