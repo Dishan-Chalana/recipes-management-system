54e9655a8bb267fe8baa503ed5e74cf5b67851cf
Dishan Chalana <dishanchalana1999@gmail.com>
2023-06-25 15:54:59 +0530

    New Recipe adding , deleting , retreving function done.

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,41 +1,64 @@
 import Table from 'react-bootstrap/Table';
 import './home.css';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 
 function Home() {
+
+    const [recipeData, setRecipeData]=useState([]);
+
+       //get data from backend
+       useEffect(()=>{
+        axios.get("http://localhost:8000")
+        .then((response)=>{
+            setRecipeData(response.data);
+        }).catch((err)=>{
+            console.log("Can not get data ", err);
+        })
+    },[])
+
+    //delete recipe
+    const handleDelete=(id)=>{
+        axios.delete(`http://localhost:8000/delete/${id}`).then((response)=>{
+          
+          alert(response.data);
+          setRecipeData(recipeData.filter((recipe)=>recipe._id !==id));
+  
+        }).catch((error)=>{
+          alert(error)
+        })
+  
+
+  
+    }
     return (
         <div className='home-container'>
             <Table striped bordered hover variant="light">
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>Recipe Name</th>
-                        <th>Ingredients</th>
-                        <th>Description</th>
-                        <th>Actions</th>
+                        <th >#</th>
+                        <th >Recipe Name</th>
+                        <th >Ingredients</th>
+                        <th >Description</th>
+                        <th >Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>
-                            <Button variant="primary">Edit</Button>{' '}
-                            <Button variant="danger">Delete</Button>{' '}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>
-                            <Button variant="primary">Edit</Button>{' '}
-                            <Button variant="danger">Delete</Button>{' '}
-                        </td>
-                    </tr>
+
+                    {recipeData.map((recipe,index)=>(
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>{recipe.name}</td>
+                            <td>{recipe.ingredients}</td>
+                            <td>{recipe.description}</td>
+                            <td>
+                                <Button variant="primary">Edit</Button>{' '}
+                                <Button variant="danger" onClick={()=>handleDelete(recipe._id)}>Delete</Button>{' '}
+                            </td>
+                        </tr>
+                    ))}
+     
                 </tbody>
             </Table>
         </div>
--- src/components/new-recipe/index.jsx
+++ src/components/new-recipe/index.jsx
@@ -1,24 +1,44 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './new-recipe.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function New() {
+     //set form data
+  const[recipeData, setRecipeData]=useState({});
+
+  const handleChange=(e)=>{
+    setRecipeData({...recipeData,[e.target.name]:e.target.value});
+  };
+
+  //handle submit recipe data
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios.post('http://localhost:8000/add',recipeData).then((response)=>{
+      alert(response.data);
+
+    }).catch((error)=>{
+      console.log(error)
+
+    });
+}
     return (
         <div class="new-container">
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="name">
                     <Form.Label>Recipe Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter name" />
+                    <Form.Control type="text" placeholder="Enter name" name="name" onChange={handleChange} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="ingredients">
                     <Form.Label>Ingredients</Form.Label>
-                    <Form.Control type="text" placeholder="Ingredients" />
+                    <Form.Control type="text" placeholder="Ingredients" name="ingredients" onChange={handleChange} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="desc">
                     <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Description" />
+                    <Form.Control type="text" placeholder="Enter Description" name="description" onChange={handleChange} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
